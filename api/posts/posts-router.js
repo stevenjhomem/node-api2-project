bab92de2d35053d04c92123da6e3e0e5174aa9b9
stevenjhomem <sjhomem@outlook.com>
2021-09-28 13:28:57 -0500

    finished setting up the project and getting the first couple of tests to pass

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,21 @@
 // implement your posts router here
+const express = require('express');
+const postsFunctions = require('./posts-model')
+const postsRouter = express.Router();
+
+
+
+postsRouter.get('/', async (req, res)=>{
+    try{
+         const posts = await postsFunctions.find()
+         res.status(201).json(posts)
+    }
+    catch(err){
+        res.status(500).json({ 
+            message: "The posts information could not be retrieved" 
+        })
+    }
+})
+
+
+module.exports = postsRouter;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
-// implement your server here
-// require your posts router and connect it here
+//Imports
+const express = require('express');
+const postsRouter = require('./posts/posts-router.js')
+
+//Instance of Express App
+const server = express();
+
+
+server.use('/api/posts', postsRouter);//Sets the root URL as /api/posts for the function. So, inside of my postsRouter function, the root URL "/" will be equivalent to /api/posts.
+
+
+//Global Middleware
+server.use(express.json());//teaches express to parse request bodies as JSON//
+
+//Exposing the server to other modules
+module.exports = server;
