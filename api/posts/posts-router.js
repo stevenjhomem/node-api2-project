5105709502692c9d66c0d394ee9b1adf65502886
stevenjhomem <sjhomem@outlook.com>
2021-09-28 14:48:58 -0500

    got some more tests to pass

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -40,5 +40,59 @@ postsRouter.get('/:id', async (req,res)=>{
 })
 //tests 3 and 4//
 
+//tests 5, 6, and 7//
+postsRouter.post('/', async (req,res)=>{
+    try{
+        const {title, contents} = req.body;
+
+        if(!title || !contents){
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        }
+        else{
+            const newPost = await postsFunctions.insert({title, contents})
+            res.status(201).json(newPost)
+        }
+    }
+    catch(err){
+        res.status(500).json({
+            message: "There was an error while saving the post to the database"
+        })
+    }
+})
+//tests 5, 6, and 7//
+
+//tests 8, 9, 10, and 11//
+postsRouter.put('/:id', async (req,res)=>{
+    try{
+        const id = req.params.id;
+        const {title, contents} = req.body;
+    
+        const idPost = await postsFunctions.findById(id);
+        
+        if(!idPost){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }
+        else if(!title || !contents){
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        }
+        else{
+            const updatedPost = await postsFunctions.update(id, {title, contents})
+            res.status(200).json(updatedPost)
+        }
+    }
+    catch(err){
+        res.status(500).json({
+            message: "The post information could not be modified"
+        })
+    }
+})
+//tests 8, 9, 10, and 11//
+
 
 module.exports = postsRouter;
--- api/server.js
+++ api/server.js
@@ -5,12 +5,13 @@ const postsRouter = require('./posts/posts-router.js')
 //Instance of Express App
 const server = express();
 
+//Global Middleware
+server.use(express.json());//teaches express to parse request bodies as JSON//
 
 server.use('/api/posts', postsRouter);//Sets the root URL as /api/posts for the function. So, inside of my postsRouter function, the root URL "/" will be equivalent to /api/posts.
 
 
-//Global Middleware
-server.use(express.json());//teaches express to parse request bodies as JSON//
+
 
 //Exposing the server to other modules
 module.exports = server;
