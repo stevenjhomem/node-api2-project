363475a0d8e32f045a2dadb6e6f666acca5e25e4
stevenjhomem <sjhomem@outlook.com>
2021-09-28 13:39:19 -0500

    finished tests 3 and 4

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,7 +4,7 @@ const postsFunctions = require('./posts-model')
 const postsRouter = express.Router();
 
 
-
+//tests 1 and 2//
 postsRouter.get('/', async (req, res)=>{
     try{
          const posts = await postsFunctions.find()
@@ -16,6 +16,29 @@ postsRouter.get('/', async (req, res)=>{
         })
     }
 })
+//tests 1 and 2//
+
+//tests 3 and 4//
+postsRouter.get('/:id', async (req,res)=>{
+    try{
+        const id = req.params.id;
+        const idPost = await postsFunctions.findById(id);
+        if(!idPost){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }
+        else{
+            res.status(201).json(idPost)
+        }
+    }
+    catch(err){
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    }
+})
+//tests 3 and 4//
 
 
 module.exports = postsRouter;
